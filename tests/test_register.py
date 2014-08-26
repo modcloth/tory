7c1a252b928a877a179b6183efbb640544ec5874
Dan Buch <d.buch@modcloth.com>
2014-08-26 17:46:19 -0400

    Stubbing out the ipv4 addr correctly in tests

--- tests/test_register.py
+++ tests/test_register.py
@@ -15,10 +15,15 @@ def fake_put_host(server, auth_token, host_def):
     return 200
 
 
+def fake_get_local_ipv4():
+    return '127.0.0.1'
+
+
 def setup():
     os.environ.clear()
     mp = monkeypatch()
     mp.setattr(tory_register, '_put_host', fake_put_host)
+    mp.setattr(tory_register, '_get_local_ipv4', fake_get_local_ipv4)
 
 
 def test_main():
