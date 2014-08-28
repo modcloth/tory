34e593e3bf1d0040806841ac0efe5f823c0014ac
Dan Buch <d.buch@modcloth.com>
2014-08-28 17:14:34 -0400

    Adding remaining python package bits + travis pypi deployment setup
    including little shim at the top level to support the nested client
    package.

--- client/setup.py
+++ client/setup.py
@@ -4,9 +4,27 @@ from setuptools import setup
 
 
 def main():
+    desc = 'client tools for the tory ansible inventory'
     setup(
         name='tory-client',
+        url='https://github.com/modcloth/tory',
+        author='ModCloth, Inc.',
+        author_email='platformsphere+pypi@modcloth.com',
+        description=desc,
+        long_description=desc,
         version=_get_version(),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: System :: Systems Administration',
+            'Topic :: Utilities',
+        ],
         py_modules=[
             'tory_inventory',
             'tory_register',
